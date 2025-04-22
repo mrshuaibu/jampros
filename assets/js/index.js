e6babb847283c16128e50e9ab453e3097ed1a9ff
Michael Shuaibu <shuaibumikel@gmail.com>
2025-04-22 14:51:21 -0500

    FAQ section added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,7 @@
 'use strict';
 const menuIcon = document.querySelector('.menu-icon');
 const navList = document.querySelector('.nav-list');
-const valueDisplays = document.querySelectorAll('.stat-number');
-const totalDuration = 2000; // 2 seconds for fast counting
 
-const startCounting = (entries, observer) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            let startValue = 0;
-            let endValue = parseInt(entry.target.getAttribute("data-val"));
-            let increment = Math.ceil(endValue / (totalDuration / 25)); // Ensures fast counting
-            let counter = setInterval(() => {
-                startValue += increment;
-                if (startValue > endValue) startValue = endValue;
-                entry.target.textContent = startValue.toLocaleString(); // Adds commas
-                if (startValue === endValue) {
-                    clearInterval(counter);
-                }
-            }, 25); // Updates every 25ms for smooth animation
-            observer.unobserve(entry.target); // Stops observing after animation starts
-        }
-    });
-};
-
-const observer = new IntersectionObserver(startCounting, { threshold: 0.5 });
-
-valueDisplays.forEach((valueDisplay) => {
-    observer.observe(valueDisplay);
-});
 
 menuIcon.addEventListener('click', function() {
     console.log('Menu icon clicked'); 
@@ -39,3 +13,16 @@ document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', 
     menuIcon.classList.remove('active');
     navList.classList.remove('active');
 }));
+
+document.addEventListener('DOMContentLoaded', () => {
+    const faqItems = document.querySelectorAll('.faq-item');
+  
+    faqItems.forEach(item => {
+      const question = item.querySelector('.faq-question');
+  
+      question.addEventListener('click', () => {
+        item.classList.toggle('active');
+      });
+    });
+  });
+  
