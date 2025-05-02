d1d0db5857d84791a5b2e38866ca6198c9b859a5
Michael Shuaibu <shuaibumikel@gmail.com>
2025-05-02 13:42:18 -0500

    about page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
 'use strict';
 const menuIcon = document.querySelector('.menu-icon');
 const navList = document.querySelector('.nav-list');
+const valueDisplays = document.querySelectorAll('.stat-number');
+const totalDuration = 2000; // 2 seconds for fast counting
 
 
 menuIcon.addEventListener('click', function() {
@@ -24,5 +26,30 @@ document.addEventListener('DOMContentLoaded', () => {
         item.classList.toggle('active');
       });
     });
-  });
-  
+});
+
+const startCounting = (entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            let startValue = 0;
+            let endValue = parseInt(entry.target.getAttribute("data-val"));
+            let increment = Math.ceil(endValue / (totalDuration / 25)); 
+            let counter = setInterval(() => {
+                startValue += increment;
+                if (startValue > endValue) startValue = endValue;
+                entry.target.textContent = startValue.toLocaleString();
+                if (startValue === endValue) {
+                    clearInterval(counter);
+                }
+            }, 25);
+            observer.unobserve(entry.target); 
+        }
+    });
+};
+
+const observer = new IntersectionObserver(startCounting, { threshold: 0.5 });
+
+valueDisplays.forEach((valueDisplay) => {
+    observer.observe(valueDisplay);
+});
+
